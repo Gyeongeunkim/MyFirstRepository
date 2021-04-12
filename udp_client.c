ec3806b8b145bc5058d9331d16cae87716572ddb
Gyeongeunkim <ggyyoonnii@naver.com>
2021-04-12 21:55:34 +0100

    week7client

---
+++ udp_client.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define BUFSIZE 30
+
+void error_handling(char *message);
+
+int main(int argc, char *argv[])
+{
+	int clint_sock;
+	char message[BUFSIZE];
